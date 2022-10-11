81eb546123263e3bde2b0b98a5010849e28130b8
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2022-10-11 14:33:10 +0530

    add basic info on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,21 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <NavBar selectedItem={selectedItem.current} />
+      <div className={styles.infoContainer}>
+        <div className={styles.avatarContainer}>
+          <img
+            src={"https://avatars.githubusercontent.com/u/45332121"}
+            className={styles.avatar}
+            alt=""
+          />
+        </div>
+        <div className={styles.greetingContainer}>
+          <div className={styles.greeting}>Hello</div>
+          <div className={styles.greetingSub}>
+            A Software Engineer based in UP, IN
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
