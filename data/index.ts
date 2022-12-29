5c575703f4a97045bb071f79640522b75b8591d9
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2022-12-29 11:43:18 +0530

    add more projects and technologies

--- data/index.ts
+++ data/index.ts
@@ -2,13 +2,39 @@ import { ITechIcons } from "./types";
 
 export const techIcons: ITechIcons = {
   c: "/c.svg",
+  cpp: "/cpp.svg",
   python: "/python.svg",
   kotlin: "/kotlin.svg",
   android: "/android.svg",
   sdl: "/sdl.svg",
+  react_native: "/react.svg",
+  typescript: "/typescript.svg",
+  gtk: "/gtk.svg",
+  qt: "/qt.svg",
+  javascript: "/javascript.svg",
 };
 
 export const projects = [
+  {
+    name: "RN OpenStreetMaps",
+    techUsed: ["react_native", "typescript"],
+    description:
+      "A React Native app using OpenStreetMap's web version disguised as an in-app thingy",
+    sourceUrl: "https://github.com/gaurav712/ReactNativeOpenStreetMaps",
+  },
+  {
+    name: "Kitchen Diary",
+    techUsed: ["react_native", "typescript"],
+    description: "An application to create and manage a catalogue of recipes",
+    sourceUrl: "https://github.com/gaurav712/Kitchen-Diary",
+  },
+  {
+    name: "BGMI Customs",
+    techUsed: ["android", "kotlin"],
+    description:
+      "An app to reserve slots for public custom rooms in battle royales",
+    sourceUrl: "https://github.com/gaurav712/BGMICustoms",
+  },
   {
     name: "status",
     techUsed: ["c"],
@@ -34,5 +60,86 @@ export const projects = [
     techUsed: ["c", "sdl"],
     description: "tool to convert images to sketch",
     sourceUrl: "https://github.com/gaurav712/sketcher",
+  },
+  {
+    name: "wlan_toggle_service",
+    techUsed: ["c"],
+    description: "UNIX service to run as super-user to be able to toggle wlan",
+    sourceUrl: "https://github.com/gaurav712/wlan_toggle_service",
+  },
+  {
+    name: "GiGS Wifi",
+    techUsed: ["python", "gtk"],
+    description: "Graphical frontend to wpa_supplicant",
+    sourceUrl: "https://github.com/gaurav712/GiGS_Wifi",
+  },
+  {
+    name: "wifi-qt",
+    techUsed: ["cpp", "qt"],
+    description: "Successor to GiGS_Wifi written in Qt5 using C++",
+    sourceUrl: "https://github.com/gaurav712/wifi-qt",
+  },
+  {
+    name: "list_mountable_stuff",
+    techUsed: ["c"],
+    description: "Lists devices that can be mounted on linux systems",
+    sourceUrl: "https://github.com/gaurav712/list_mountable_stuff",
+  },
+  {
+    name: "subtitle_parser",
+    techUsed: ["c"],
+    description:
+      "parses *.srt files and spits out dialogues at relevant intervals",
+    sourceUrl: "https://github.com/gaurav712/subtitle_parser",
+  },
+  {
+    name: "brightness",
+    techUsed: ["c"],
+    description: "utility to control brightness on linux systems",
+    sourceUrl: "https://github.com/gaurav712/brightness",
+  },
+  {
+    name: "cpufreq",
+    techUsed: ["c"],
+    description: "utility to set maximum cpu frequency on linux systems",
+    sourceUrl: "https://github.com/gaurav712/cpufreq",
+  },
+  {
+    name: "alsavolcontrol",
+    techUsed: ["c"],
+    description: "control volume using ALSA",
+    sourceUrl: "https://github.com/gaurav712/alsavolcontrol",
+  },
+  {
+    name: "whatsapp_chat_emulation",
+    techUsed: ["python", "gtk"],
+    description: "emulates whatsapp chat screen from exported chats",
+    sourceUrl: "https://github.com/gaurav712/whatsapp_chat_emulation",
+  },
+  {
+    name: "meet_auto_admit",
+    techUsed: ["javascript"],
+    description:
+      "Chrome extention to automatically admit people to a Google Meet call",
+    sourceUrl: "https://github.com/gaurav712/meet_auto_admit",
+  },
+  {
+    name: "arduino_morse",
+    techUsed: ["cpp"],
+    description:
+      "converts a given string to Morse Code using arduino's inbuilt LED",
+    sourceUrl: "https://github.com/gaurav712/arduino_morse",
+  },
+  {
+    name: "ebook_finder",
+    techUsed: ["python", "gtk"],
+    description: "to find free ebooks online",
+    sourceUrl: "https://github.com/gaurav712/ebook_finder",
+  },
+  {
+    name: "launch",
+    techUsed: ["c"],
+    description: "an alternative to xdg-open",
+    sourceUrl: "https://github.com/gaurav712/launch",
   },
 ];
