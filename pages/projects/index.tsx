e542ee5949c1b668ba9662fea4380ac76d7ae48f
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-03-15 13:52:19 +0530

    make the beta projects page the default

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,8 +1,37 @@
 import Head from "next/head";
-import { useRef } from "react";
+import { CSSProperties, useRef } from "react";
 import NavBar from "../../components/NavBar";
-import { projects, techIcons } from "../../data";
+import ProjectsList from "../../data/projects";
+import Icons from "../../data/icons";
 import styles from "./styles.module.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+const timelineStyles: { [key: string]: CSSProperties } = {
+  timelineContent: {
+    backgroundColor: "#282828",
+    boxShadow: "none",
+    top: "-20px",
+    padding: "1rem auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  contentArrow: {
+    borderRightColor: "#282828",
+  },
+  timelineIcon: {
+    backgroundColor: "#fff",
+    width: "1.5rem",
+    height: "1.5rem",
+    boxShadow: "none !important",
+    marginLeft: "-0.75rem",
+  },
+};
 
 const Projects = () => {
   const selectedItem = useRef("Projects");
@@ -20,21 +49,18 @@ const Projects = () => {
             <div className={styles.additionalInfo}>
               <div className={styles.techUsedInfo}>
                 <span className={styles.tooltip}>
-                  <img
-                    src={techIcons.nextjs.iconSource}
-                    className={styles.logo}
-                  />
+                  <img src={Icons.nextjs.iconSource} className={styles.logo} />
                   <span className={styles.tooltipText}>
-                    {techIcons.nextjs.tooltipText}
+                    {Icons.nextjs.tooltipText}
                   </span>
                 </span>
                 <span className={styles.tooltip}>
                   <img
-                    src={techIcons.typescript.iconSource}
+                    src={Icons.typescript.iconSource}
                     className={styles.logo}
                   />
                   <span className={styles.tooltipText}>
-                    {techIcons.typescript.tooltipText}
+                    {Icons.typescript.tooltipText}
                   </span>
                 </span>
               </div>
@@ -57,41 +83,61 @@ const Projects = () => {
             </div>
           </div>
           <div className={styles.projectsList}>
-            {projects.map((item, index) => (
-              <div key={`${index}`} className={styles.projectInfoContainer}>
-                <div className={styles.projectTitle}>{item.name}</div>
-                <div className={styles.additionalInfo}>
-                  <div className={styles.techUsedInfo}>
-                    {item.techUsed.map((tech, index) => (
-                      <span key={`${index}`} className={styles.tooltip}>
-                        <img
-                          src={techIcons[tech].iconSource}
-                          className={styles.logo}
-                        />
-                        <span className={styles.tooltipText}>
-                          {techIcons[tech].tooltipText}
-                        </span>
-                      </span>
+            <VerticalTimeline animate={false} lineColor="grey">
+              {Object.keys(ProjectsList).map((month, index) => (
+                <VerticalTimelineElement
+                  key={index}
+                  date={`${month}`}
+                  contentStyle={timelineStyles.timelineContent}
+                  contentArrowStyle={timelineStyles.contentArrow}
+                  iconStyle={timelineStyles.timelineIcon}
+                  dateClassName={styles.timelineDate}
+                >
+                  <>
+                    {ProjectsList[month].map((item, key) => (
+                      <div key={key} className={styles.projectInfoContainer}>
+                        <div className={styles.projectTitle}>{item.name}</div>
+                        <div className={styles.additionalInfo}>
+                          <div className={styles.techUsedInfo}>
+                            {item.techUsed.map((tech, index) => (
+                              <span key={`${index}`} className={styles.tooltip}>
+                                <img
+                                  src={Icons[tech].iconSource}
+                                  className={styles.logo}
+                                />
+                                <span className={styles.tooltipText}>
+                                  {Icons[tech].tooltipText}
+                                </span>
+                              </span>
+                            ))}
+                          </div>
+                          <div
+                            className={styles.additionalInfoText}
+                            dangerouslySetInnerHTML={{
+                              __html: item.description,
+                            }}
+                          />
+                        </div>
+                        <div className={styles.techUsedInfo}>
+                          <img
+                            src="/github.svg"
+                            className={styles.githubLogo}
+                          />
+                          <a
+                            target={"_blank"}
+                            rel="noreferrer"
+                            className={styles.sourceUrl}
+                            href={item.sourceUrl}
+                          >
+                            {item.sourceUrl}
+                          </a>
+                        </div>
+                      </div>
                     ))}
-                  </div>
-                  <div
-                    className={styles.additionalInfoText}
-                    dangerouslySetInnerHTML={{ __html: item.description }}
-                  />
-                </div>
-                <div className={styles.techUsedInfo}>
-                  <img src="/github.svg" className={styles.githubLogo} />
-                  <a
-                    target={"_blank"}
-                    rel="noreferrer"
-                    className={styles.sourceUrl}
-                    href={item.sourceUrl}
-                  >
-                    {item.sourceUrl}
-                  </a>
-                </div>
-              </div>
-            ))}
+                  </>
+                </VerticalTimelineElement>
+              ))}
+            </VerticalTimeline>
           </div>
         </div>
       </div>
