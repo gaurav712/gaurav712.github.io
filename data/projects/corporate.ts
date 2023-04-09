b8f7061609011b08970a81e01bd9efce2a686a3b
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2023-04-09 11:20:21 +0530

    Add projects to the 'Professional' tab

--- data/projects/corporate.ts
+++ data/projects/corporate.ts
@@ -1,6 +1,64 @@
 import { IProjectsList } from "../types";
 
 const projects: IProjectsList = {
+  "Feb, 2023": [
+    {
+      name: "Lilly Design System",
+      techUsed: ["nextjs"],
+      description:
+        'A UI components\' library by <i><a style="display: inline;" href="https://www.lilly.com">Lilly</a></i> using NextJS',
+      sourceUrl: "https://designsystem.lilly.com",
+    },
+  ],
+  "Dec, 2022": [
+    {
+      name: "HMS",
+      techUsed: ["react"],
+      description:
+        "A webapp to manage everything <i>medical</i>, i.e patients, appointments etcetra.",
+      sourceUrl: "_",
+    },
+  ],
+  "Sep, 2022": [
+    {
+      name: "Demodex Blepharitis AR",
+      techUsed: ["nextjs"],
+      description:
+        "An AR based webapp using <b>NextJS</b> for a conference by Tarsus Pharmaceuticals, Inc. on <i><b>Demodex</b></i> blepharitis",
+      sourceUrl: "https://tarsusrx.com/demodex-blepharitis",
+    },
+  ],
+  "Mar, 2022": [
+    {
+      name: "Sova Health",
+      techUsed: ["react_native"],
+      description:
+        '<i>"Sova Health is a precision nutrition platform that guides users towards a healthier lifestyle with personalized nutrition advice based on accurate blood biomarker analysis. Understand what is stopping you from achieving your nutritional goal and get your personalized nutrition plan."</i>',
+      sourceUrl: "https://play.google.com/store/apps/details?id=com.sovaapp",
+    },
+  ],
+  "Jan, 2022": [
+    {
+      name: "Double Tap (previously TapTap)",
+      techUsed: ["react_native"],
+      description: "An app to share your profile using NFC",
+      sourceUrl: "https://dbltap.com.au",
+    },
+  ],
+  "Dec, 2021": [
+    {
+      name: "NudgeID",
+      techUsed: ["react_native"],
+      description: "An app to share contact info using NFC",
+      sourceUrl: "https://nudgeid.com/pages/download",
+    },
+    {
+      name: "Ntouch",
+      techUsed: ["react_native"],
+      description: "An app to share contact info using NFC",
+      sourceUrl: "https://www.youtube.com/watch?v=a90VoJ2ZXZ0",
+    },
+  ],
   "Nov, 2021": [
     {
       name: "SIPFund",
